aee03aa71255e920e0f7e61ceaee3a489b7accb7
Breno Macedo <128511253+Br-76@users.noreply.github.com>
2023-06-07 17:04:57 -0300

    Add files via upload

---
+++ Bibliotecas/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.red(`Fuck You world`))
+
+console.log(`São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar`)
